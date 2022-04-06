1f0799fb42832d1a9be9ca944f49258c8094fec8
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-04-06 16:17:26 +0700

    initial commit

---
+++ src/test/java/com/nazyli/chatappservices/ChatAppServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazyli.chatappservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
